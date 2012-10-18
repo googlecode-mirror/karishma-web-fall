3fc132e458a74400758a7bfba857ec5eb2a61c34
unknown <anne@.(none)>
2012-10-18 16:08:48 -0400

    hi

--- Final/inc/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('password.php');
-   function GetConnection()
-   {
-   global $password;
-   $conn=new mysqli('localhost','n02701310',$password,'n02701310_db');
-   return $conn;
-           }
-?>
---
+++ Final/views/Users/delete.php
@@ -0,0 +1,23 @@
+<?
+require_once ('../../models/Users.php');
+$results = Users::GetAll();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<? include('../../inc/head.php'); ?>
+	<body>
+		<div>
+			<? include('../../inc/nav.php'); ?>
+
+			<div id="content">
+
+
+
+				
+			</div>
+			<? include('../../inc/footer.php'); ?>
+		</div>
+	</body>
+</html>
