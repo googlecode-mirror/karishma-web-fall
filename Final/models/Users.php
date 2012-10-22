ba6793cdc95326f723a07932c7c080d1e9a729ce
unknown <anne@.(none)>
2012-10-22 14:30:18 -0400

    start

--- Final/models/Users.php
+++ Final/models/Users.php
@@ -1,18 +1,18 @@
 <?php
-require_once ('/home/plotkinm/WWW/2012Grad/Final/inc/functions.php');
+require_once ('/home/n02701310/WWW/Final/inc/functions.php');
 
 class Users
 {
 	static function GetAll()
 	{
 		$conn = GetConnection();
-		return $conn->query('SELECT * FROM 2012Grad_Users');
+		return $conn->query('SELECT * FROM Users');
 	}
 	
 	static function Get($id)
 	{
 		$conn = GetConnection();
-		$results = $conn->query("SELECT * FROM 2012Grad_Users WHERE id=$id");
+		$results = $conn->query("SELECT * FROM Users WHERE id=$id");
 		$row = $results->fetch_assoc();
 		$conn->close();
 		return $row;
@@ -25,7 +25,7 @@ class Users
 	static function Update($row)
 	{
 		$conn = GetConnection();
-		$sql = 	"UPDATE 2012Grad_Users "
+		$sql = 	"UPDATE Users "
 			.	"Set FirstName='$row[FirstName]',LastName='$row[LastName]',created_at='$row[created_at]',updated_at='$row[updated_at]',Keyword_id='$row[Keyword_id]' "
 			.	"WHERE id=$row[id] ";
 		//echo $sql;
