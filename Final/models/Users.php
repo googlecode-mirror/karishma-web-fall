5fb48cf7f998386df59018a9007c28e147de483c
unknown <anne@.(none)>
2012-10-23 16:17:26 -0400

    start

--- Final/models/Users.php
+++ Final/models/Users.php
@@ -3,41 +3,52 @@ require_once ('/home/n02701310/WWW/Final/Final/inc/functions.php');
 
 class Users
 {
-	static function GetAll()
-	{
-		$conn = GetConnection();
-		return $conn->query('SELECT * FROM Users');
-	}
-	
-	static function Get($id)
-	{
-		$conn = GetConnection();
-		$results = $conn->query("SELECT * FROM Users WHERE id=$id");
-		$row = $results->fetch_assoc();
-		$conn->close();
-		return $row;
-	}
+        static function GetAll()
+        {
+                $conn = GetConnection();
+                return $conn->query('SELECT * FROM Users');
+        }
+        
+        static function Get($id)
+        {
+                $conn = GetConnection();
+                $results = $conn->query("SELECT * FROM Users WHERE id=$id");
+                $row = $results->fetch_assoc();
+                $conn->close();
+                return $row;
+        }
 
-	static function Insert()
-	{
-	}
-	
-	static function Update($row)
-	{
-		$conn = GetConnection();
-		$sql = 	"UPDATE Users "
-			.	"Set UserId='$row[UserId]',FirstName='$row[FirstName]',LastName='$row[LastName]',Email='$row[Email]',OrderId=$row[OrderId]', "
-			.	"WHERE id=$row[id] ";
-		echo $sql;
-		$conn->query($sql);
-		$error = $conn->error;
-		$conn->close();
-		
-		return $error != '' ? array('Server Error' => $error) : true;		
-	}
-	
-	static function Delete()
-	{
-	}
+        static function Insert()
+        {
+        }
+        
+        static function Update($row)
+        {
+                $conn = GetConnection();
+                $row2 = EscapeRow($row, $conn);
+                $sql =  "UPDATE 2012Grad_Users "
+                        .       "Set UserId='$row2[UserId]',FirstName='$row2[FirstName]',LastName='$row2[LastName]',Email='$row2[Email]',OrderId='$row2[OrderId]' "
+                        .       "WHERE id=$row2[id] ";
+                echo $sql;
+                $conn->query($sql);
+                $error = $conn->error;
+                $conn->close();
+                
+                return $error != '' ? array('Server Error' => $error) : true;           
+        }
+        
+        static function Delete()
+        {
+        }
+        
+        static function Validate($row)
+        {
+                $results = array();
+                if(!is_numeric($row['Keyword_id'])) $results['Keyword_id'] = 'Keyword id needs to be a number';
+                if(empty($row['Keyword_id'])) $results['Keyword_id'] = 'Keyword is required';
+                if(empty($row['FirstName'])) $results['FirstName'] = 'FirstName is required';
+                if(empty($row['LastName'])) $results['LastName'] = 'LastName is required';
+                
+                return count($results) > 0 ? $results : true;
+        }
 }
-
