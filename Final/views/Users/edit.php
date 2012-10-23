7a47933a99d98a41dfa06047480246e9b7265e26
unknown <anne@.(none)>
2012-10-23 14:52:13 -0400

    start

--- Final/views/Users/edit.php
+++ Final/views/Users/edit.php
@@ -1,20 +1,20 @@
 <?
 require_once ('../../models/Users.php');
 
-if(isset($_POST['id']))
+if(isset($_POST['user_id']))
 {
 	$row = $_POST;
 	$response = Users::Validate($row);
-	if($response === true)
-	$response = Users::Update($row);
+	//echo "asd"+$response;
+	//$response=true;
+	
+	if($response === true)	
+		$response = Users::Update($row);
 	if($response === true)
 		header("Location: index.php");
 }else{
 	$row = Users::Get($_REQUEST['id']);
 }
-
-
-
 
 ?>
 
@@ -37,40 +37,38 @@ if(isset($_POST['id']))
 					</dl>
 				<? endif; ?>
 				<form class="form-horizontal" action="" method="post">
-					<input type="hidden" name="id" value="<?=$row['id']?>" />
-					<div class="control-group">
-						<label class="control-label">UserId:</label>
-						<div class="controls">
-							<input type="text" name="Keyword_id" value="<?=$row['UserId:']?>" />
-						</div>
-					</div>
+					<input type="hidden" name="user_id" value="<?=$row['user_id']?>" />
 					<div class="control-group">
 						<label class="control-label" for="FirstName">First Name:</label>
 						<div class="controls">
-							<input type="text" name="FirstName" id="FirstName" value="<?=$row['FirstName']?>" />
+							<input 	type="text" name="FirstName" id="FirstName" value="<?=$row['FirstName']?>"
+								   	class="<?=isset($response['FirstName']) ? 'error' : '' ?>"
+							/>
+							<? if(isset($response['FirstName'])): ?>
+								<span class="error"><?=$response['FirstName']?></span>
+							<? endif; ?>
 						</div>
 					</div>
 					
 					<div class="control-group">
 						<label class="control-label">Last Name:</label>
 						<div class="controls">
-							<input type="text" name="LastName" value="<?=$row['LastName']?>" />
+							<input type="text" name="LastName" value="<?=$row['LastName']?>" class="required" />
 						</div>
 					</div>
 				
 					<div class="control-group">
-						<label class="control-label">Email:</label>
+						<label class="control-label">created_at:</label>
 						<div class="controls">
-							<input type="text" name="Email" value="<?=$row['Email']?>" />
+							<input type="text" name="user_regdate" class="tcal tcalInput" value="<?=$row['user_regdate']?>" />
 						</div>
 					</div>
 					<div class="control-group">
-						<label class="control-label">Order Id</label>
+						<label class="control-label">updated_at:</label>
 						<div class="controls">
-							<input type="text" name="OrderId" value="<?=$row['OrderId']?>" />
+							<input type="text" name="user_last_login" class="tcal tcalInput" value="<?=$row['user_last_login']?>" />
 						</div>
 					</div>
-					
 					
 					<div class="control-group">
 						<div class="controls">
@@ -83,5 +81,20 @@ if(isset($_POST['id']))
 			</div>
 			<? include('../../inc/footer.php'); ?>
 		</div>
+		<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.10.0/jquery.validate.min.js"></script>
+		<script type="text/javascript">
+			$(function(){
+				
+				$("form").validate(
+					{
+						rules: { created_at: { required: true} }
+					}
+				);
+				
+				$("dl").hide();
+				$("dl").slideDown("slow");
+				
+			});
+		</script>
 	</body>
 </html>
