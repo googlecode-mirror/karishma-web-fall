e036a2e0e5ef9943e8b7d6ea375783f7fa6df3ca
unknown <New Paltz@.newpaltz.edu>
2012-11-05 16:00:01 -0500

    start

--- Final/views/Users/index.php
+++ Final/views/Users/index.php
@@ -12,15 +12,21 @@ $results = Users::GetAll();
                         <? include('../../inc/nav.php'); ?>
 
                         <div id="content">
-                        
+                        <? if(isset($_GET['inserted'])): ?>
+                                    <div class="alert alert-success">
+                                            <button type="button" class="close">×</button>
+                                            A user has been successfuly added.
+                                    </div>
+                                <? endif; ?>
+
                         	<a href="edit.php">+Create New</a>
                                 <table class="table table-bordered table-condensed table-hover table-striped">
-                                        <tr>
+                                        <tr  >
                                                 <th>First Name</th><th>Last Name</th><th>Actions</th>
                                         </tr>
                                         
                                         <? while($row = $results->fetch_assoc()): ?>
-                                                <tr>
+                                                <tr class="<?= isset($_GET['inserted']) && $row['id'] == $_GET['inserted'] ? 'error' : '' ?>">
                                                         <td><?=$row['FirstName']?></td> <td><?=$row['LastName']?> </td>
                                                         <td>
                                                                 <a href="details.php?id=<?=$row['id']?>">Details</a>
@@ -35,5 +41,46 @@ $results = Users::GetAll();
                         </div>
                         <? include('../../inc/footer.php'); ?>
                 </div>
+       <div id="delete-box" class="modal hide fade">
+                            <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h3>Delete</h3>
+                            </div>
+                            <div class="modal-body">
+                                    
+                            </div>
+                    </div>
+                    <script type="text/javascript" src="../../static/js/bootstrap.js"></script>
+                <script type="text/javascript">
+                        $(function(){
+                                $(".close").click(function(){
+                                        $(this).closest(".alert").slideUp();
+                                });
+                                $(".error").removeClass('error', 'slow');
+                                $(".delete-link").click(function(){
+                                        $("#delete-box").modal({'show':true });
+                                        $.get(this.href, function(data){
+                                                $("#delete-box").find(".modal-body").html($(data).find("form"));
+                                        });
+                                        return false;
+                                });
+                        });
+                </script>
+         
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
         </body>
 </html>
