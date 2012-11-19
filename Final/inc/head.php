9db1832f9b665e77c223af276503899307b00b24
unknown <New Paltz@.newpaltz.edu>
2012-11-19 16:43:24 -0500

    start

--- Final/inc/head.php
+++ Final/inc/head.php
@@ -1,6 +1,6 @@
 	<head>
 		<meta charset="utf-8" />
-		<title>Index</title>
+		<title>My Store</title>
 		<link type="text/css" rel="stylesheet" href="../../static/css/bootstrap.min.css" />
 		<link type="text/css" rel="stylesheet" href="../../static/css/main.css" />
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
